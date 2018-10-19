2169bce781bdd083d6c80265690407e78e0d2707
argossy <argossy@gmail.com>
2018-10-19 14:23:09 -0400

    Add files via upload

---
+++ slurm_submit/run_R_code.R
@@ -0,0 +1,17 @@
+## command line in interactive mode
+# srun R CMD BATCH --slave "--args lasso 1" run_R_code.R
+
+
+### use another R code or shell script to submit R job in batch
+## command line in batch mode to submit multiple jobs
+# sbatch --ntasks=1 --time=1:00 --mem=10 --wrap="srun R CMD BATCH --slave '--args lasso 1' run_R_code.R"
+
+args = commandArgs(TRUE)
+
+dataset = args[1]
+i = as.numeric(args[2])
+
+# i = 1; dataset='lasso';
+mx1 = matrix(rnorm(20*10),20,10,byrow=TRUE)
+write.csv(mx1,file=sprintf('mx_data_%s_%s.csv',dataset,i),row.names=FALSE,quote=FALSE)
+
---
+++ slurm_submit/submit_batch_R_code.R
@@ -0,0 +1,12 @@
+## this code submit R job with arguments in for loop using SLURM on longleaf.unc.edu
+
+## submit run_R_code.R with different input parameters and runs in different jobs
+
+dataset = 'lasso'
+for(i in 1:10){
+	wrap_cmd = sprintf("srun R CMD BATCH --slave '--args %s %s' run_R_code.R", dataset, i)
+	cmd1 = sprintf("sbatch --ntasks=1 --time=1:00 --mem=10 --wrap=\"%s\"",wrap_cmd)
+	print(cmd1)
+	system(cmd1)
+}
+
